0ab0586b105d686b74d5d3c66c617e8bb39e8c2d
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-08-15 10:58:00 -0500

    started outline for app, add login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
-
+import Login from "./Login.js"
 function App() {
   return (
     <div className="app">
       
+      {/* spotify logo */}
+      {/* login with spotify button */}
+      <Login />
+
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            {/* spotify logo */}
+            {/* login with spotify button */}
+        </div>
+    )
+}
+
+export default Login
