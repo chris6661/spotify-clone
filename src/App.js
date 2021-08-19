dc53fecd23480b96858e01acbea91ac1a2b46a6a
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-08-19 00:09:56 -0500

    added spotify web js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { useEffect } from "react"; 
+import { WindowsToaster } from "node-notifier";
+import React, { useEffect, useState } from "react"; 
 import './App.css';
 import Login from "./login/Login";  
 import { getTokenFromUrl } from "./spotify"; 
 function App() {
 
+  const [token, setToken] = useState(null); 
   //run code based on a given condition
   useEffect(() => {
-    const token = getTokenFromUrl(); 
-    console.log((token))
+    const hash = getTokenFromUrl(); 
+    window.location.hash = ""; 
+    const _token = hash.access_token; 
+
+    if(_token) {
+      //if there is a token, set token to be token 
+      setToken(_token)
+    }
+
+    console.log("HERE IS YOUR TOKEN", token)
   }, []); 
 
   return (
     <div className="app">
+      {
+        //if there is a token, will show logged in page.player, if not token then will go to login page
+        token ? (
+          <h1>logged innnn</h1>
+        ) : (
+          <Login />
+        )
+      }
       
-      <Login />
 
     </div>
   );
