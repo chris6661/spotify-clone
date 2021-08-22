51528780eaadfef3a84d2ecdf06b45b50509f59c
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-08-22 11:05:11 -0500

    added player component, added in to appjs file as well

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { WindowsToaster } from "node-notifier";
 import React, { useEffect, useState } from "react"; 
 import './App.css';
 import Login from "./login/Login";  
+import Player from "./player/Player"; 
 import { getTokenFromUrl } from "./spotify"; 
+import SpotifyWebApi from "spotify-web-api-js"; 
+
+const spotify = new SpotifyWebApi(); 
 function App() {
 
   const [token, setToken] = useState(null); 
@@ -12,9 +15,16 @@ function App() {
     window.location.hash = ""; 
     const _token = hash.access_token; 
 
+    //use an underscore to use multiple variables with same name, in this case token 
     if(_token) {
       //if there is a token, set token to be token 
       setToken(_token)
+
+      spotify.setAccessToken(_token); 
+
+      spotify.getMe().then((user) => {
+        console.log(user)
+      })
     }
 
     console.log("HERE IS YOUR TOKEN", token)
@@ -24,12 +34,9 @@ function App() {
     <div className="app">
       {
         //if there is a token, will show logged in page.player, if not token then will go to login page
-        token ? (
-          <h1>logged innnn</h1>
-        ) : (
-          <Login />
-        )
-      }
+        token ?
+        <Player />
+         : <Login /> }
       
 
     </div>
