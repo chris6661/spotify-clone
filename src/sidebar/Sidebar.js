177eb6c9465b5bb87898e8262656b9081bb9e7de
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-08-31 10:46:56 -0500

    added material ui, added title text to sidebar

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './sidebar.css'; 
-
+import SidebarOption from "./SidebarOption"; 
 function Sidebar() {
     return (
         <div className="sidebar">
             <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify logo" />
+
+            <SidebarOption title="Home" />
+            <SidebarOption title="Search" />
+            <SidebarOption title="Your Library" />
+
 
         </div>
     )
