4de19913aab778153ec30a75570d7708d2190236
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-09-01 10:34:35 -0500

    added icons to sidebar from material ui

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './sidebar.css'; 
 import SidebarOption from "./SidebarOption"; 
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+
 function Sidebar() {
     return (
         <div className="sidebar">
