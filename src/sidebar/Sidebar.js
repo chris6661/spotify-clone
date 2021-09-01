43a18083e26932ca262f71e0f3f5e4272d789494
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-09-01 10:55:27 -0500

    stlying for sidebar completed

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -9,11 +9,14 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify logo" />
+                {/* pass icons in as props here */}
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
 
-            <SidebarOption title="Home" />
-            <SidebarOption title="Search" />
-            <SidebarOption title="Your Library" />
-
+        <br />
+        <strong className="sidebar_title">PLAYLISTS</strong>
+        <hr />
 
         </div>
     )
--- src/sidebar/SidebarOption.js
+++ src/sidebar/SidebarOption.js
@@ -4,7 +4,11 @@ import "./sidebarOption.css";
 function SidebarOption({ title, Icon }) {
     return (
         <div className="sidebarOption">
-            <p>{title}</p>
+        {/* two children in a container, the container being sidebarOption, name of this div  */}
+            {Icon && <Icon className="sidebarOption_icon" /> }
+            {/* if there is an icon show icon with h4, if no icon then render title */}
+           {Icon ? <h4>{title}</h4> : <p>{title}</p> }
+
         </div>
     )
 }
